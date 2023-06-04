e54b3fac7ccb07926bb87eaf947521693d9b9eda
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-04 22:10:43 +0200

    fix typo and eslint error

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,9 @@ const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?"
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 render(() => <App />, root!);
