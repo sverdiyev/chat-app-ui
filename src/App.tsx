9996df6fc019fab9f4080228b5c99e118975c352
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-04 22:22:20 +0200

    finalize prettier tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,8 @@ import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div class="bg-orange-950">
-      <header class="bg-orange-950">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class="bg-orange-950"
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="rounded bg-orange-50 px-4 py-2 font-bold text-black">
+      empty app
     </div>
   );
 };
